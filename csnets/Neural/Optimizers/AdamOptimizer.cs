6a4a90e3e54a6499ca9b3dda6a7256e4b9ac2a00
ppisod <hicnkebromzih@gmail.com>
2026-03-16 14:54:53 +0800

    AdamOptimizer.cs MomentumOptimizer.cs csnets.csproj

---
+++ csnets/Neural/Optimizers/AdamOptimizer.cs
@@ -0,0 +1,7 @@
+namespace csnets.Neural.Optimizers;
+
+public class AdamOptimizer ( float momentum ) : MomentumOptimizer ( momentum ) {
+
+    
+
+}
--- csnets/Neural/Optimizers/MomentumOptimizer.cs
+++ csnets/Neural/Optimizers/MomentumOptimizer.cs
@@ -3,13 +3,14 @@ namespace csnets.Neural.Optimizers;
 public class MomentumOptimizer ( float momentum ) : IOptimizer {
     private readonly Dictionary<Weight, float> weightVelocities = new ();
 
-    public void ApplyGradient ( Weight weight, float gradient, float learningRate ) {
+    public virtual void ApplyGradient ( Weight weight, float gradient, float learningRate ) {
         float existingVelocity = weightVelocities.GetValueOrDefault ( weight, 0 );
         existingVelocity = (momentum * existingVelocity) - (gradient * learningRate);
         weight.value += existingVelocity;
         weightVelocities[weight] = existingVelocity;
     }
-    public void ApplyGradients ( Weight weight, float learningRate ) {
+
+    public virtual void ApplyGradients ( Weight weight, float learningRate ) {
         float avgGrads = weight.AverageGradient ();
         float existingVelocity = weightVelocities.GetValueOrDefault ( weight, 0 );
         existingVelocity = (momentum * existingVelocity) - (avgGrads * learningRate);
