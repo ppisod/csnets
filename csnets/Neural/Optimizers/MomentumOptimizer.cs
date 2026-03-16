e29bde464909631669d522e8ded2fbd4ca220cb5
ppisod <hicnkebromzih@gmail.com>
2026-03-16 14:40:27 +0800

    MomentumOptimizer.cs

--- csnets/Neural/Optimizers/MomentumOptimizer.cs
+++ csnets/Neural/Optimizers/MomentumOptimizer.cs
@@ -1,5 +1,13 @@
 namespace csnets.Neural.Optimizers;
 
-public class MomentumOptimizer {
+public class MomentumOptimizer : IOptimizer {
+
     
+
+    public void UpdateWeight ( Weight weight, float gradient, float learningRate ) {
+        throw new NotImplementedException ();
+    }
+    public void ApplyGradients ( Weight weight, float learningRate ) {
+        throw new NotImplementedException ();
+    }
 }
